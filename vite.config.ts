38889f299cca89aeb2968eec813c2bffd6ec2203
mohammad rida <mhmd_rida_990@hotmail.com>
2025-05-21 21:49:00 +0300

    increase build chunks

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // raise the warning threshold from 500 KB to 1000 KB
+  },
 });
